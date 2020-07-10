3b86f5100d084690c34d899217ddf1c0e0ea501d
Dave-56 <pemakenemi56@gmail.com>
2020-07-10 16:34:54 -0400

    commiting next changes

--- server.js
+++ server.js
@@ -7,7 +7,29 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  //   console.log('a user connected');
+
+  //create helper function to send status
+  const sendStatus = (s) => {
+    socket.emit('status', s);
+  };
+  socket.on('input', (data) => {
+    let name = data.name;
+    let message = data.message;
+
+    //send status error
+    if (!name || !message) {
+      sendStatus('Please enter a name and message');
+    }
+    io.emit('chat message', [data]);
+
+    //send status object
+    sendStatus({
+      message: 'Message sent',
+    });
+
+    // io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, () => console.log('Server started on *:3000'));
